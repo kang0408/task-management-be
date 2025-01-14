90c161c6a5da5ae515500414188c59a5660f179b
Kang0408 <trandanhkhang482004@gmail.com>
2025-01-14 18:11:06 +0700

    add user login

--- api/v1/routes/user.routes.js
+++ api/v1/routes/user.routes.js
@@ -3,6 +3,10 @@ const controller = require("../controllers/user.controller");
 
 const router = express.Router();
 
+// Cần validate
 router.post("/register", controller.register);
+
+// Cần validate
+router.post("/login", controller.login);
 
 module.exports = router;
